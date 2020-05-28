88a887664a3958bbab36f146f4ecd0d147e60018
SergioDDiaz <61720992+SergioDDiaz@users.noreply.github.com>
2020-05-28 13:13:36 -0500

    Ciclo de camaras

---
+++ SwitchCam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCam : MonoBehaviour
+{
+    private int a = 0;
+    public Camera cam1;
+    public Camera cam2;
+    // Start is called before the first frame update
+    public void swcm(int x)
+    {
+        
+ 
+
+        DesCam();
+        if (a == 0)
+        {
+            cam1.enabled = true;
+            a += x;
+        }
+        else if (a == 1)
+        {
+            cam2.enabled = true;
+            a -= x;
+        }
+        
+
+    }
+    public void DesCam()
+    {
+        cam1.enabled = false;
+        cam2.enabled = false;
+
+    }
+  }
