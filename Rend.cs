d8f5f596c405884d36879cf66d86e4a86ccfc4fc
SergioDDiaz <61720992+SergioDDiaz@users.noreply.github.com>
2020-06-02 11:32:56 -0500

    Visualiza2

--- Rend.cs
+++ Rend.cs
@@ -7,17 +7,20 @@ public class Rend : MonoBehaviour
     public GameObject ojeto1;
     public GameObject ojeto2;
     public GameObject ojeto3;
+    public GameObject ojeto4;
 
     public void enable()
     {
         ojeto1.SetActive(true);
         ojeto2.SetActive(true);
         ojeto3.SetActive(true);
+        ojeto4.SetActive(false);
     }
     public void disable()
     {
         ojeto1.SetActive(false);
         ojeto2.SetActive(false);
         ojeto3.SetActive(false);
+        ojeto4.SetActive(true);
     }
 }
